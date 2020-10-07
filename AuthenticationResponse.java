e0f4e2be2c22f1d374408daba136554a0a140f0f
yathishnaik <62278555+yathishnaik@users.noreply.github.com>
2020-10-07 17:01:40 +0530

    Add files via upload

---
+++ AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package models;
+
+public class AuthenticationResponse {
+	private final String jwt;
+	
+
+	public AuthenticationResponse(String jwt) {
+		this.jwt = jwt;
+	}
+
+
+	public String getJwt() {
+		return jwt;
+	}
+
+}
---
+++ SpringSecurityHardApplication.java
@@ -0,0 +1,13 @@
+package com.example.secureweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityHardApplication.class, args);
+	}
+
+}
